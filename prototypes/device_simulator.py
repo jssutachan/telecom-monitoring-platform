6185b2c83075750188020de13fcb0a64fe458227
Juan Sebastián Sutachán Correa <jsebastiansc935@gmail.com>
2026-03-27 18:59:30 -0500

    feat: enhance simulator with latency and battery metrics

--- prototypes/device_simulator.py
+++ prototypes/device_simulator.py
@@ -4,9 +4,13 @@ import logging
 from datetime import datetime
 
 # --- Constants (Calibration) ---
+MIN_LATENCY_RANGE = 50  # ms
+MAX_LATENCY_RANGE = 500  # ms
+LATENCY_THRESHOLD = 300  # ms
 MIN_RSSI = -100
 MAX_RSSI = 0
 RSSI_THRESHOLD = -80
+BATTERY_CRITICAL_LEVEL = 15.0  # percentage
 TOTAL_READINGS = 10
 SLEEP_INTERVAL = 1  # seconds
 
@@ -17,15 +21,28 @@ def get_device_id() -> int:
     """Generates a random unique identifier for a device."""
     return random.randint(1, 1000)
 
+def get_status(rssi: int, latency: int, battery_level: float) -> str:
+    """Determines the status of the device based on telemetry data."""
+    if battery_level < BATTERY_CRITICAL_LEVEL:
+        return "CRITICAL"
+    elif rssi < RSSI_THRESHOLD or latency > LATENCY_THRESHOLD:
+        return "WARNING"
+    else:
+        return "OK"
+
 def generate_reading(device_id: int) -> dict:
     """Creates a single telemetry data point"""
-    rssi = random.randint(MIN_RSSI, MAX_RSSI)
-    
+    rssi: int = random.randint(MIN_RSSI, MAX_RSSI)
+    latency: int = random.randint(MIN_LATENCY_RANGE, MAX_LATENCY_RANGE)  # ms
+    battery_level: float = round(random.uniform(0, 100),2)  # percentage
+
     return {
         "device_id": device_id,
         "timestamp": datetime.now().isoformat(),
         "rssi": rssi,
-        "status": "OK" if rssi > RSSI_THRESHOLD else "WARNING"
+        "latency": latency,
+        "battery_level": battery_level,
+        "status": get_status(rssi, latency, battery_level)
     }
 
 def simulate_device_readings(device_id: int) -> None:
@@ -33,9 +50,9 @@ def simulate_device_readings(device_id: int) -> None:
     logging.info(f"Starting Simulation for Device ID: {device_id}")
     
     try:
-        for i in range(1, TOTAL_READINGS + 1):
+        for i in range(TOTAL_READINGS):
             reading = generate_reading(device_id)
-            logging.info(f"Reading {i}/{TOTAL_READINGS}: {reading}")
+            logging.info(f"Device {device_id} RSSI: {reading['rssi']}, Latency: {reading['latency']}, Battery Level: {reading['battery_level']}, Status: {reading['status']}")
             time.sleep(SLEEP_INTERVAL)
             
     except KeyboardInterrupt:
